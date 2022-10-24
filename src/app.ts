d25b4fb7c58a27d3e65b34956e35d9d1e252c3d7
Paweł Niedużak <pawel.nieduzak@icloud.com>
2022-10-24 15:02:53 +0200

    finished project

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-if (window.innerWidth > 768)
-	window.addEventListener('resize', () => {
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	});
-console.log(window.innerWidth);
